b84d487e28feb1d2aff29c3226e5504508ebf486
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-08-29 22:55:47 +0900

    Add: 掲示板作成ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # localhost:3000にアクセスが来た場合のルーティングを意味する。
   # 以下の場合はBoardsControllerクラスのindexメソッドを実行するように定義されている
-  root 'boards#index'
+  # GETメソッドで/boardsにリクエストが来た場合にBoardsControllerのindexアクションが呼ばれるように定義
+  get 'boards', to: 'boards#index'
+  # GETメソッドで/boards/newにリクエストが来た場合、BoardsControllerのnewアクションが呼ばれるように定義
+  get 'boards/new', to: 'boards#new'
 end
