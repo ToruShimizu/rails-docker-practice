b3d278c5b77e8b81664b5785595b341a674b9774
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-08-29 14:24:56 +0900

    Add: 掲示板ページを作成

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,9 @@
+# コントローラーを定義する際は基本的にApplicationControllerクラスを継承する
+class BoardsController < ApplicationController
+    # indexメソッド
+    # rootに定義したメソッドのことをアクションという
+    # BoardsControllerのindexアクションとも呼ばれる
+    def index
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # localhost:3000にアクセスが来た場合のルーティングを意味する。
+  # 以下の場合はBoardsControllerクラスのindexメソッドを実行するように定義されている
+  root 'boards#index'
 end
