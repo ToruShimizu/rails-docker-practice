b3e79786e8e5bcd86b8a85bc32544cb2440aef89
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-08-30 22:52:58 +0900

    Add: 掲示板に投稿する機能を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,6 +11,17 @@ class BoardsController < ApplicationController
         # Boardモデルのオブジェクトを作成して@boardというインスタンス変数に代入
         # インスタンス変数はビューでも参照できる
         @board = Board.new
+    end
+    
+    def create
+        Board.create(board_params)
         binding.pry
+    end
+
+    private 
+# paramsメソッドの中のboardキーの中のname title bodyのキーを取得できる
+    def board_params
+        # model名のキーとモデルのプロパティのキー
+        params.require(:board).permit(:name, :title, :body)
     end
 end
