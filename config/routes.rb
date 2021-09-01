19f03644032adfb6d1d35a2a94d012e35ec88ebb
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-01 23:56:48 +0900

    Add: resourcesを使ったルーティング処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # localhost:3000にアクセスが来た場合のルーティングを意味する。
-  # 以下の場合はBoardsControllerクラスのindexメソッドを実行するように定義されている
-  # GETメソッドで/boardsにリクエストが来た場合にBoardsControllerのindexアクションが呼ばれるように定義
-  get 'boards', to: 'boards#index'
-  # GETメソッドで/boards/newにリクエストが来た場合、BoardsControllerのnewアクションが呼ばれるように定義
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
-  get 'boards/:id', to: 'boards#show'
+  resources :boards, only: [:index, :new, :create, :show]
 end
