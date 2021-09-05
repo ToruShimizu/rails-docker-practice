6969cd0daf884a5818dbb7098eb70668b70a498e
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-05 17:11:48 +0900

    Add: ユーザー認証に必要なルーティング処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # mypageにアクセスされた時はusersのmeアクションが呼ばれる
+  get 'mypage', to: 'users#me'
+  # ログインというURLで行う
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'users/new'
 
   get 'users/create'
@@ -13,6 +19,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create, destroy]
 end
