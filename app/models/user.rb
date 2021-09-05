cc6d8fb8c39643f7988f08134ecd6e5f96af7eff
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-05 17:19:15 +0900

    Add: ユーザー名入力のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,20 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+    # ユーザー名の入力を必須
+    presence: true,
+    # ユーザー名の重複は禁止
+    uniqueness: true,
+    # 文字の長さ
+    length: { maximum: 16 },
+    # 正規表現
+    format: {
+      with: /\A[a-z0¥9]+\z/,
+      message: 'は小文字英数字で入力してください。'
+    }
+  validates :password,
+    length: { maximum: 8 }
 end
