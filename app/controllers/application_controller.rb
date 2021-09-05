30785ba65c5e2b26fb914656d43ce19b04f8cd80
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-05 17:51:17 +0900

    Add: ログインユーザーの情報の取得処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  beefore_action :current_user
+
+  private
+
+  def current_user
+    # セッションにユーザIDの情報がなければ処理から抜ける、@userのインスタンス変数はnilになる
+    return unless session[:user_id]
+    # ログイン中のユーザーが代入される  
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
