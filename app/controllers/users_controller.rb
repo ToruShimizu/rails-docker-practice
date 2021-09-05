0e77e3f8370cbb55d5e23ea42770b20b76c27f6d
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-05 17:32:32 +0900

    Add: ユーザー登録機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
   def new
     # 新しいユーザーを登録するだけ
-    @user = User.new
+    @user = User.new(flash([:user]))
   end
 
   def create
+    # フォームに送信された新しいparamsから新しいユーザーオブジェクトを作成
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to :back, flash: {
+        user: user,
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
