da6ec60dfadff9f54767f75391d7af501680e72f
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-05 17:45:38 +0900

    Add: ログイン、ログアウト機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
   def create
+    # 引数に指定した条件とマッチするユーザーを一件取得する
+    # 該当しない場合はnilが返ってくる
+    user = User.find_by(name: params[:session][:name])
+    # ユーザーがnilではなく、パスワードがマッチして認証が成功している
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
